3ad17e8b7aeb89c66b5520b972864bbbe1c9ab11
Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>
2020-12-19 12:34:15 +0800

    Try problem matcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'test.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    try {
     final x = "";
+    } catch (e) {}
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
