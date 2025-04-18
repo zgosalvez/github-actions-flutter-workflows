41e520a0d5cd1efaa7663685c176397b23de3b5a
Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>
2025-04-18 09:26:53 +0800

    Recreate application

--- android/app/src/main/kotlin/com/example/github_actions_flutter_workflows/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/github_actions_flutter_workflows/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.github_actions_flutter_workflows
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
